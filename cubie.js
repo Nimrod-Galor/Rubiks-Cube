da564a6ef0ffe9889fb2dbd0f6789820c2942048
unknown <galor_nimrod@hotmail.com>
2024-06-17 19:51:15 +0300

    plain rotation

--- cubie.js
+++ cubie.js
@@ -1,19 +1,33 @@
 class Cubie{
-    constructor(_matrix){
-        this.matrix = _matrix;
+    constructor(_x, _y, _z){
+        this.matrix = [];//_matrix;
+        this.x = _x;
+        this.y = _y;
+        this.z = _z;
+        this.hiegh = false;
     }
 
     show(){
         // Begin the drawing group.
         push();
-        fill(55);
-        stroke(1.0);
-        //strokeWeight(0.01);
+        fill(255);
+        if(this.height){
+            fill(255, 0, 0);
+        }
+        stroke(1);
+        strokeWeight(4);
         //pushMatrix();
         applyMatrix(this.matrix);
         box(10);
         //resetMatrix();
         // End the drawing group.
         pop();
+    }
+
+    updateMatrix(){
+        // this.x = _x;
+        // this.y = _y;
+        // this.z = _z;
+        this.matrix = [10, 0, 0, 0, 0, 10, 0, 0, 0, 0, 10, 0, 100 * this.x, 100 * this.y, 100 * this.z, 1];
     }
 }
--- main.js
+++ main.js
@@ -7,14 +7,50 @@ function setup() {
     for(let x = -1; x<=1; x++){
         for(let y = -1; y<=1; y++){
             for(let z = -1; z<=1; z++){
-                var matrix = [10, 0, 0, 0, 0, 10, 0, 0, 0, 0, 10, 0, 100 * x, 100 * y, 100 * z, 1];
-                cube[index] = new Cubie(matrix);
+                //var matrix = [10, 0, 0, 0, 0, 10, 0, 0, 0, 0, 10, 0, 100 * x, 100 * y, 100 * z, 1];
+                //cube[index] = new Cubie(matrix, x, y, z);
+                cube[index] = new Cubie(x, y, z);
+                cube[index].updateMatrix();
                 index++;
             }
         }
     }
+
+    cube[2].height = true;
+//    turnZ();
   }
   
+  function plainRotation(plain, depth, dir){
+        let plainMatrix = [];
+        let plainArr = cube.filter(item => item[plain] === depth);
+
+        let index = 0;
+        //convert plain array to matrix
+        for(let r = 0; r<=2; r++){
+            plainMatrix[r] = [];
+            for(let c = 0; c<=2; c++){
+                plainMatrix[r][c] = {x:plainArr[index].x, y:plainArr[index].y, z:plainArr[index].z};
+                index++;
+            }
+        }
+
+        // rotate matriz
+        plainMatrix = dir === 1 ? plainMatrix[0].map((val, index) => plainMatrix.map(row => row[index]).reverse()) : plainMatrix[0].map((val, index) => plainMatrix.map(row => row[row.length-1-index]));
+
+        // update cube object
+        index = 0;
+        for(let r = 0; r<=2; r++){
+            for(let c = 0; c<=2; c++){
+                plainArr[index].x = plainMatrix[r][c].x;
+                plainArr[index].y = plainMatrix[r][c].y;
+                plainArr[index].z = plainMatrix[r][c].z
+                plainArr[index].updateMatrix();
+
+                index++;
+            }
+        }
+  }
+
   function draw() {
     background(200);
     
